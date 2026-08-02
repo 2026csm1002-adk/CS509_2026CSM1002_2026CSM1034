8892610357750e493e9bb571420e5e7f6df8e4fb
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-02 16:38:56 +0530

    bfs and dfs code

---
+++ assignment_01/src/bfs.cpp
@@ -0,0 +1,27 @@
+vector<int> bfsTraversal(vector<int> &row_ptr, vector<int> &col_idx, int src){
+    vector<int> bfs;
+    int V = row_ptr.size() -1;
+    queue<int> que;
+
+    vector<int> visited(V,0);
+    que.push(src);
+    visited[src]=1;
+
+    while(!que.empty()){
+        int node = que.front();
+        que.pop();
+        bfs.push_back(node);
+        int start = row_ptr[node];
+        int end = row_ptr[node+1];
+
+        for(int i=start_idx; i<end_idx; i++){
+            int neigh = col_idx[i];
+            if(!visited[neigh]){
+                visited[neigh]=1;
+                que.push(neigh);
+            }
+        }
+    }
+
+    return bfs;
+}
---
+++ assignment_01/src/dfs.cpp
@@ -0,0 +1,26 @@
+vector<int> dfsTraversal(vector<int> &row_ptr, vector<int> &col_idx, int src){
+    vector<int> dfs;
+    int V = row_ptr.size()-1;
+    stack<int> stk; //stack
+
+    vector<int> visited(V,0);
+    stk.push(src);
+    visited[node]=1;
+    while(!stk.empty()){
+        int node = stk.top();
+        stk.pop();
+        dfs.push_back(node);
+        int start = row_ptr[node];
+        int end = row_ptr[node+1];
+
+        for(int i=end-1; i>=start; i--){
+            int neigh = col_idx[i];
+            if(!visited[neigh]){
+                visited[neigh]=1;
+                stk.push(neigh);
+            }
+        }
+    }
+
+    return dfs;
+}
