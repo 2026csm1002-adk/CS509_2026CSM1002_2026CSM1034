acba93614666599d6469bacc19b00e58ef5f2b6e
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-04 19:38:47 +0530

    utilities.h files created

---
+++ common_wrapper/utilities.h
@@ -0,0 +1,58 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include<iostream>
+#include<vector>
+#include<filesystem>
+#include<fstream>
+
+using namespace std;
+namespace fs = filesystem;
+
+//storing the files available in that folders
+inline vector<string> getTestsFiles(const string &folderPath){
+    vector<string> files;
+
+    for(const auto &entry : fs::directory_iterator(folderPath)){
+        if(entry.path().extension() == ".txt"){
+            files.push_back(entry.path().string());
+        }
+    }
+
+    return files;
+}
+
+//showing tests files in that folder
+inline void showTests(const vector<string> &files){
+    cout << "\nAvailable Test Files: \n\n";
+
+    for(int i=0; i<files.size(); i++){
+        cout<<i+1<<". "<<fs::path(files[i]).filename().string()<<endl;
+    }
+}
+
+// this for running the testfiles
+inline string chooseFiles(const string &folderPath){
+    vector<string> files = getTestsFiles(folderPath);
+    showTests(files);
+
+    int choice;
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+
+    while(choice < 1 || choice > files.size()){
+        cout<<"\n Invalid choice Try Again: ";
+        cin >> choice;
+    }
+
+    return files[choice - 1];
+}
+
+inline string createOutputFiles(const string &inputFile, const string &algorithm){
+    fs::create_directories("assignment_01/outputs");
+    string filename = fs::path(inputFile).stem().string();
+
+    return "assignment_01/outputs/" + filename + "_" + algorithm + ".txt";
+}
+
+#endif //UTILITY_H
