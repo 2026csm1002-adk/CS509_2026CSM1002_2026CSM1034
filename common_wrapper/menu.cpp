fc76364c47260b0be8791fe615df66d7d77aeffb
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-04 15:51:34 +0530

    Menu folder is Completed

---
+++ common_wrapper/menu.cpp
@@ -0,0 +1,43 @@
+#include<iostream.h>
+#include<menu.h>
+
+using namespace std;
+
+int Menu::showAssignmentMenu(){
+    cout <<"\n==========================================\n";
+    cout <<"       Buddy CS509 Assignment Menu            \n";
+    cout <<"==========================================\n";
+
+    cout<<" 1. Assignment 1"<<endl;
+    cout<<" 0. Exit"<<endl;
+
+    int choose=-1;
+
+    cout<<"Enter your choice: ";
+    cin>>choose;
+    return choose;
+}
+
+int Menu::ShowAlgorithmMenu(){
+    cout<<"\n=============Algorithm Menu============="<<endl;
+    cout<<"1. BFS"<<endl;
+    cout<<"2. DFS"<<endl;
+    cout<<"3. Single Source Shortest Path"<<endl;
+    cout<<"0. Back"<<endl;
+    int choose=-1;
+    cout<<"Enter your choice: ";
+    cin>>choose;
+    return choose;
+}
+
+int Menu::showInputMenu(){
+    cout<<"\n=============Input Menu============="<<endl;
+    cout<<"1. Run Selected Test "<<endl;
+    cout<<"2. Run all Tests Cases"<<endl;
+    cout<<"3. Run Custom Test"<<endl;
+    cout<<"0. Back"<<endl;
+    int choose=-1;
+    cout<<"Enter your choice: ";
+    cin>>choose;
+    return choose;
+}
---
+++ common_wrapper/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+
+class Menu{
+    
+public:
+    int showAlgorithMenu();
+    int showAssignmentMenu();
+    int showTestTypeMenu();
+};
+
+#endif //MENU_H
