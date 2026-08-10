df791d72c8c2c2ff02a778e37d2a2432f9225a40
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-10 19:47:49 +0530

    traingle header file included

---
+++ assignment_02/src/triangle_counting/tri_count.h
@@ -0,0 +1,16 @@
+#ifndef TRI_COMP_H
+#define TRI_COMP_H
+
+#include<vector>
+#include<iostream>
+#include "../../../assignment_02/src/csr/csr.h"
+using namespace std;
+struct TriangleResult{
+    long long totalTriangles=0;
+    vector<array<int, 3>> triangles;
+};
+
+TriangleResult countTriangle(CSRGraph &g);
+
+
+#endif //TRI_COMP_H
