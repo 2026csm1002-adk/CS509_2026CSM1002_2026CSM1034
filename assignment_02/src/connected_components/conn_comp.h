a172a63ff7f1a9e9c273abea50d2349970a8b484
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-10 18:38:13 +0530

    implemented connected componenets header file

---
+++ assignment_02/src/connected_components/conn_comp.h
@@ -0,0 +1,14 @@
+#ifndef CONNECTED_COMPONENTS_H
+#define CONNECTED_COMPONENTS_H
+
+// Function prototypes for connected components
+#include<vector>
+#include<iostream>
+using namespace std;
+struct ComponentsResult{
+    int numComponents =0;
+    vector<int> component;
+};
+ComponentsResult count_connected_components(CSRGGraph &g);
+
+#endif // CONNECTED_COMPONENTS_H
