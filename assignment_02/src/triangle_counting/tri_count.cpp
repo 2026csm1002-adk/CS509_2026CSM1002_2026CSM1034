da2420360e3ae24fbe067f23412cd399bdd2a83b
lightweightcobra <adityakumaradk5@gmail.com>
2026-08-10 22:10:24 +0530

    Minor change

--- assignment_02/src/triangle_counting/tri_count.cpp
+++ assignment_02/src/triangle_counting/tri_count.cpp
@@ -1,25 +1,36 @@
 #include "tri_count.h"
-#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include <array>
+
+using namespace std;
 TriangleResult countTriangle(CSRGraph &g){
     TriangleResult result;
+    set<array<int,3>> unique_triangles;
 
-    unordered_set<array<int,3>> unique_triangles;
-    for(int u=0; u<g.V; u++){
-
-        for(int i=g.row_ptr[u]; i<g.row_ptr[u+1]; i++){
+    for (int u = 0; u < g.V; u++) {
+        for (int i = g.row_ptr[u]; i < g.row_ptr[u+1]; i++) {
             int v = g.col_idx[i];
-            for(int j=g.row_ptr[v]; j<g.row_ptr[v+1]; j++){
+            if (v == u) continue;
+
+            for (int j = g.row_ptr[v]; j < g.row_ptr[v+1]; j++) {
                 int w = g.col_idx[j];
-                if(u != v && v != w && u != w){
-                    array<int,3> triset = {u, v, w};
-                    sort(triset.begin(), triset.end());
-                    if(unique_triangles.find(triset) == unique_triangles.end()){
-                        unique_triangles.insert(triset);
-                        result.triangles.push_back(triset);
-                        result.totalTriangles++;
-                    }
+                if (w == u || w == v) continue;
+
+                // check that edge u-w actually exists
+                bool uw_edge = std::binary_search(
+                    g.col_idx.begin() + g.row_ptr[u],
+                    g.col_idx.begin() + g.row_ptr[u+1],
+                    w
+                );
+                if (!uw_edge) continue;
+
+                array<int,3> triset = {u, v, w};
+                sort(triset.begin(), triset.end());
+                if (unique_triangles.insert(triset).second) {
+                    result.triangles.push_back(triset);
+                    result.totalTriangles++;
                 }
-                
             }
         }
     }
